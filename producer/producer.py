4ebfddb31de0ad0ecbca7722c6891a91c081658f
Admin199633 <108216254+Admin199633@users.noreply.github.com>
2022-07-12 21:21:37 +0300

    Update producer.py

--- producer/producer.py
+++ producer/producer.py
@@ -2,48 +2,56 @@ import pika, logging, sys, argparse
 from argparse import RawTextHelpFormatter
 from time import sleep
 
-if __name__ == '__main__':
-    examples = sys.argv[0] + " -p 5672 -s localhost -m 'Hello' "
-    parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter,
-                                 description='Run producer.py',
-                                 epilog=examples)
-    parser.add_argument('-p', '--port', action='store', dest='port', help='The port to listen on.')
-    parser.add_argument('-s', '--server', action='store', dest='server', help='The RabbitMQ server.')
-    parser.add_argument('-m', '--message', action='store', dest='message', help='The message to send', required=False, default='Hello')
-    parser.add_argument('-r', '--repeat', action='store', dest='repeat', help='Number of times to repeat the message', required=False, default='30')
 
-    args = parser.parse_args()
-    if args.port == None:
-        print ("Missing required argument: -p/--port")
-        sys.exit(1)
-    if args.server == None:
-        print ("Missing required argument: -s/--server")
-        sys.exit(1)
 
-    # sleep a few seconds to allow RabbitMQ server to come up
-    sleep(5)
+def my_send():
+        examples = sys.argv[0] + " -p 5672 -s localhost -m 'Hello' "
+        parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter,
+                                     description='Run producer.py',
+                                     epilog=examples)
+        parser.add_argument('-p', '--port', action='store', dest='port', help='The port to listen on.')
+        parser.add_argument('-s', '--server', action='store', dest='server', help='The RabbitMQ server.')
+        parser.add_argument('-m', '--message', action='store', dest='message', help='The message to send', required=False, default='Hello')
+        parser.add_argument('-r', '--repeat', action='store', dest='repeat', help='Number of times to repeat the message', required=False, default='30')
 
-    logging.basicConfig(level=logging.INFO)
-    LOG = logging.getLogger(__name__)
-    credentials = pika.PlainCredentials('admin', 'secretpassword')
-    parameters = pika.ConnectionParameters(args.server,
-                                           int(args.port),
-                                           '/',
-                                           credentials)
-    connection = pika.BlockingConnection(parameters)
-    channel = connection.channel()
-    q = channel.queue_declare('pc')
-    q_name = q.method.queue
+        args = parser.parse_args()
+        if args.port == None:
+            print ("Missing required argument: -p/--port")
 
-    # Turn on delivery confirmations
-    channel.confirm_delivery()
+            sys.exit(1)
+        if args.server == None:
+            print ("Missing required argument: -s/--server")
+            sys.exit(1)
 
-    for i in range(0, int(args.repeat)):
-        if channel.basic_publish('', q_name, args.message):
-            LOG.info('Message has been delivered')
-        else:
-            LOG.warning('Message NOT delivered')
+        # sleep a few seconds to allow RabbitMQ server to come up
+        sleep(5)
 
-        sleep(2)
+        logging.basicConfig(level=logging.INFO)
+        LOG = logging.getLogger(__name__)
+        credentials = pika.PlainCredentials('admin', 'Lior12345')
+        parameters = pika.ConnectionParameters(args.server,
+                                               int(args.port),
+                                               '/',
+                                               credentials)
+        connection = pika.BlockingConnection(parameters)
+        channel = connection.channel()
+        q = channel.queue_declare('pc')
+        q_name = q.method.queue
 
-    connection.close()
+        # Turn on delivery confirmations
+        channel.confirm_delivery()
+
+        for i in range(0, int(args.repeat)):
+            if channel.basic_publish('', q_name, args.message):
+                LOG.info('Message has been delivered')
+            else:
+                LOG.warning('Message NOT delivered')
+
+            sleep(1)
+
+        connection.close()
+
+
+while (0==0):
+    my_send()
+    sleep(20)

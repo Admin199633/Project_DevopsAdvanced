dd24884847b891ea5c50006668eff6fc6e987d53
Admin199633 <108216254+Admin199633@users.noreply.github.com>
2022-07-15 20:25:30 +0300

    Update producer.py

--- producer/producer.py
+++ producer/producer.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
 
     logging.basicConfig(level=logging.INFO)
     LOG = logging.getLogger(__name__)
-    credentials = pika.PlainCredentials('guest', 'guest')
+    credentials = pika.PlainCredentials('user', 'Lior12345')
     parameters = pika.ConnectionParameters(args.server,
                                            int(args.port),
                                            '/',

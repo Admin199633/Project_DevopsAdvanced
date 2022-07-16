4f5fde91922b1326941ac0997f020e1bbe03e580
Admin199633 <108216254+Admin199633@users.noreply.github.com>
2022-07-16 18:44:19 +0300

    Update expose-grafana.py

--- expose-grafana.py
+++ expose-grafana.py
@@ -1,2 +1,2 @@
 import os
-os.system('cmd /k " kubectl port-forward --namespace monitoring svc/rabbitmq 3000:3000"')
+os.system('cmd /k " kubectl port-forward --namespace monitoring svc/grafana 3000:3000"')

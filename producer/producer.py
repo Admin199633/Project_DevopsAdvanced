450241347b49d807f789678572e0a67fd2b456ff
Admin199633 <108216254+Admin199633@users.noreply.github.com>
2022-07-17 20:56:45 +0300

    Update producer.py

--- producer/producer.py
+++ producer/producer.py
@@ -43,7 +43,7 @@ def producer():
             if channel.basic_publish('', q_name, args.message):
                 LOG.info('Message has been delivered')
             else:
-                LOG.warning('Message has been delivered')
+                LOG.info('Message has been delivered')
 
             sleep(13)
 
